96bae74a09e2f843e31f96af5ed3c11ee4bf1943
GnuhEht <bungphehunghi@gmail.com>
2023-10-12 04:04:05 +0700

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+import Todo from '../assets/projects/todo.png'
+import RealEst from '../assets/projects/log.png'
+import Folio from '../assets/projects/work.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Folio,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Todo,
+        github:"https://github.com/gnuheht/todolistt",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Folio,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
