9feabf388a6a2affb0ce62d2195fb63061c56beb
GnuhEht <bungphehunghi@gmail.com>
2023-10-13 22:04:00 +0700

    Edit page Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,8 +13,11 @@ const Home = () => {
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
           I'm a Front-End Developer.
+          <div className='mt-2 text-4xl sm:text-4xl text-[#ccd6f6]'>-- LESS LUCK, MORE SKILL --</div>
+
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        
         I am a newbie to front-end development with little experience. Currently, I am in my final year at the University of Transport and Communications, majoring in Information Technology.
         </p>
         <div>
